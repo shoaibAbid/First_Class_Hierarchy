05e99c567e3cc0052bb92d47d41c06ae34832963
Shoaib Abid <73983203+shoaibAbid@users.noreply.github.com>
2023-11-14 13:14:09 +0500

    Update program.py

--- program.py
+++ program.py
@@ -1,13 +1,20 @@
-
+# In program.py
 
 import hr
+import employees
+import productivity
 
-salary_employee = hr.SalaryEmployee(1, 'John Smith', 1500)
-hourly_employee = hr.HourlyEmployee(2, 'Jane Doe', 40, 15)
-commission_employee = hr.CommissionEmployee(3, 'Kevin Bacon', 1000, 250)
+manager = employees.Manager(1, 'Mary Poppins', 3000)
+secretary = employees.Secretary(2, 'John Smith', 1500)
+sales_guy = employees.SalesPerson(3, 'Kevin Bacon', 1000, 250)
+factory_worker = employees.FactoryWorker(2, 'Jane Doe', 40, 15)
+employees = [
+    manager,
+    secretary,
+    sales_guy,
+    factory_worker,
+]
+productivity_system = productivity.ProductivitySystem()
+productivity_system.track(employees, 40)
 payroll_system = hr.PayrollSystem()
-payroll_system.calculate_payroll([
-    salary_employee,
-    hourly_employee,
-    commission_employee
-])
+payroll_system.calculate_payroll(employees)
